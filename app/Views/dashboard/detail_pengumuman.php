c04dfc5e603ef35a52072df80d1bc4be015c10a9
hadidgautama <hadidpilargautama@gmail.com>
2024-08-08 15:25:25 +0700

    warna dan detail

--- app/Views/dashboard/detail_pengumuman.php
+++ app/Views/dashboard/detail_pengumuman.php
@@ -2,10 +2,14 @@
 
 <?= $this->section('content'); ?>
 
-<div class="container mt-4">
-    <h2><?= $announcement['title']; ?></h2>
-    <img src="<?= base_url('img/' . $announcement['image']); ?>" alt="Announcement Image" class="img-fluid mb-4">
-    <p><?= $announcement['description']; ?></p>
+<div class="container mt-4 bg-white p-4">
+    <h2 class="text-center"><?= $announcement['title']; ?></h2>
+    <br>
+    <div class="text-center">
+        <img src="<?= base_url('img/' . $announcement['image']); ?>" alt="Announcement Image" class="img-fluid mb-4">
+    </div>
+    <br>
+    <p class="text-center"><?= $announcement['description']; ?></p>
 </div>
 
 <?= $this->endSection(); ?>
